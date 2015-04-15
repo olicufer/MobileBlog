c1b826f2e6b854a87f4084131955ff90ec26637b
anderipartek <ander.ipartek@gmail.com>
2015-04-15 10:02:43 +0200

    actualizacion script bbdd

--- mb15.sql
+++ mb15.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-04-2015 a las 17:15:35
+-- Tiempo de generación: 15-04-2015 a las 10:01:02
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -30,9 +30,9 @@ CREATE TABLE IF NOT EXISTS `articulo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'fecha del artículo',
   `titulo` varchar(250) NOT NULL DEFAULT 'Sin titulo' COMMENT 'título del articulo',
+  `contenido` text NOT NULL,
   `id_usuario` int(11) NOT NULL,
-  `contenido` text NOT NULL COMMENT 'descripción del articulo',
-  `url_foto` varchar(255) NOT NULL,
+  `url_foto` varchar(535) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_usuario` (`id_usuario`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
@@ -41,14 +41,14 @@ CREATE TABLE IF NOT EXISTS `articulo` (
 -- Volcado de datos para la tabla `articulo`
 --
 
-INSERT INTO `articulo` (`id`, `fecha`, `titulo`, `id_usuario`, `contenido`, `url_foto`) VALUES
-(8, '2015-03-16 16:15:35', 'Nuevo articulo sobre peces', 3, '				  						  						  						  		Los peces (con nombre cientÃ­fico Pisces) son animales vertebrados acuÃ¡ticos, generalmente ectotÃ©rmicos,(regulan su temperatura a partir del medio ambiente) la mayorÃ­a de ellos recubiertos por escamas, y dotados de aletas, que permiten su movimiento continuo en los medios acuÃ¡ticos, y branquias, con las que captan el oxÃ­geno disuelto en el agua. Pisces es una superclase siendo un grupo parafilÃ©tico.\r\n\r\nLos peces son abundantes tanto en agua salada como en agua dulce, pudiÃ©ndose encontrar especies desde los arroyos de montaÃ±a (por ejemplo, el gobio), asÃ­ como en lo mÃ¡s profundo del ocÃ©ano (por ejemplo, anguilas tragonas).\r\n\r\nLos alimentos preparados con pescado son una importante fuente de nutriciÃ³n para los seres humanos. Pueden ser pescados a partir de ejemplares silvestres, o criados de manera similar al ganado (vÃ©ase acuicultura). Hoy en dÃ­a la llamada pesca deportiva cada 			  					  					  					  					  	', 'uploads/Chrysanthemum.jpg'),
-(9, '2015-03-16 16:17:15', 'PHP un lenguaje anarkiko', 1, 'bla vla bla bla ....', ''),
-(13, '2015-03-26 09:12:09', 'Billabong xxl ', 1, 'bla vla bla bla ....', ''),
-(14, '2015-03-26 09:14:18', 'Nuevo articulo333', 1, 'bla vla bla bla ....', ''),
-(17, '2015-03-26 09:32:27', 'sssseeeerrr', 1, 'bla vla bla bla ....', ''),
-(18, '2015-03-26 09:33:41', 'Java mola mucho', 3, 'bla vla bla bla ....', ''),
-(19, '2015-03-26 09:38:32', 'Recien creado', 1, 'bla vla bla bla ....', '');
+INSERT INTO `articulo` (`id`, `fecha`, `titulo`, `contenido`, `id_usuario`, `url_foto`) VALUES
+(8, '2015-03-16 16:15:35', 'Nuevo articulo sobre peces', 'Los PEZKEÃ‘IÃ‘IES (con nombre cientÃ­fico Pisces) son animales vertebrados acuÃ¡ticos, generalmente ectotÃ©rmicos,(regulan su temperatura a partir del medio ambiente) la mayorÃ­a de ellos recubiertos por escamas, y dotados de aletas, que permiten su movimiento continuo en los medios acuÃ¡ticos, y branquias, con las que captan el oxÃ­geno disuelto en el agua. Pisces es una superclase siendo un grupo parafilÃ©tico.        ', 3, '/uploads/Jellyfish.jpg'),
+(9, '2015-03-16 16:17:15', 'PHP un lenguaje anarkiko', ' lengauje anarko  ', 3, '/uploads/Desert.jpg'),
+(13, '2015-03-26 09:12:09', 'Billabong xxl ', ' The Big Wave Awards Wipeout, Surfline Overall Performance and Women''s Best Performance nominees are in and they are nothing short of legendary.', 3, ''),
+(14, '2015-03-26 09:14:18', 'Nuevo articulo333', '', 1, ''),
+(17, '2015-03-26 09:32:27', 'sssseeeerrr', ' ', 3, '/uploads/Koala.jpg'),
+(18, '2015-03-26 09:33:41', 'Java mola mucho', '', 3, ''),
+(19, '2015-03-26 09:38:32', 'Recien creado', '', 1, '');
 
 -- --------------------------------------------------------
 
