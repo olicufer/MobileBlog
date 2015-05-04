1933501df57ad0d317e9ae24c65b3de599357fc0
ivaalfo <alfonso.aparicio.ivan@gmail.com>
2015-05-04 17:21:47 +0200

    Agregada tabla categoria a la BD

--- mb15.sql
+++ mb15.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2015 a las 09:10:01
+-- Tiempo de generación: 04-05-2015 a las 17:10:21
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -32,26 +32,51 @@ CREATE TABLE IF NOT EXISTS `articulo` (
   `titulo` varchar(250) NOT NULL DEFAULT 'Sin titulo' COMMENT 'título del articulo',
   `contenido` text NOT NULL,
   `id_usuario` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
   `foto` varchar(535) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_usuario` (`id_usuario`)
+  KEY `id_usuario` (`id_usuario`),
+  KEY `id_categoria` (`id_categoria`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
 
 --
 -- Volcado de datos para la tabla `articulo`
 --
 
-INSERT INTO `articulo` (`id`, `fecha`, `titulo`, `contenido`, `id_usuario`, `foto`) VALUES
-(8, '2015-03-16 16:15:35', 'Nuevo articulo sobre pec$$$$$$', '				  						  		Los PEZKEÃ‘IÃ‘IES (con nombre cientÃ­fico Pisces) son animales vertebrados acuÃ¡ticos, generalmente ectotÃ©rmicos,(regulan su temperatura a partir del medio ambiente) la mayorÃ­a de ellos recubiertos por escamas, y dotados de aletas, que permiten su movimiento continuo en los medios acuÃ¡ticos, y branquias, con las que captan el oxÃ­geno disuelto en el agua. Pisces es una superclase siendo un grupo parafilÃ©tico.        				  					  	', 1, 'Jellyfish.jpg'),
-(9, '2015-03-16 16:17:15', 'PHP un lenguaje anarkiko', ' lengauje anarko  ', 3, 'Desert.jpg'),
-(13, '2015-03-26 09:12:09', 'Billabong xxl ', ' The Big Wave Awards Wipeout, Surfline Overall Performance and Women''s Best Performance nominees are in and they are nothing short of legendary.', 3, 'Koala.jpg'),
-(14, '2015-03-26 09:14:18', 'Nuevo articulo333', '', 1, 'Koala.jpg'),
-(17, '2015-03-26 09:32:27', 'sssseeeerrr', ' ', 3, 'Koala.jpg'),
-(18, '2015-03-26 09:33:41', 'Java mola mucho', '', 3, 'Koala.jpg'),
-(19, '2015-03-26 09:38:32', 'Recien creado', '', 1, 'Koala.jpg'),
-(20, '2015-04-30 09:22:29', 'New faro de san temlmo', '		body		  						  	', 1, 'Lighthouse.jpg'),
-(21, '2015-04-30 09:22:42', 'New faro de san temlmo', '		body		  						  	', 1, 'Lighthouse.jpg'),
-(22, '2015-04-30 09:23:05', 'New faro de san temlmo', '		body		  						  	', 1, 'Koala.jpg');
+INSERT INTO `articulo` (`id`, `fecha`, `titulo`, `contenido`, `id_usuario`, `id_categoria`, `foto`) VALUES
+(8, '2015-03-16 16:15:35', 'Nuevo articulo sobre pec$$$$$$', '				  						  		Los PEZKEÃ‘IÃ‘IES (con nombre cientÃ­fico Pisces) son animales vertebrados acuÃ¡ticos, generalmente ectotÃ©rmicos,(regulan su temperatura a partir del medio ambiente) la mayorÃ­a de ellos recubiertos por escamas, y dotados de aletas, que permiten su movimiento continuo en los medios acuÃ¡ticos, y branquias, con las que captan el oxÃ­geno disuelto en el agua. Pisces es una superclase siendo un grupo parafilÃ©tico.        				  					  	', 1, 1, 'Jellyfish.jpg'),
+(9, '2015-03-16 16:17:15', 'PHP un lenguaje anarkiko', ' lengauje anarko  ', 3, 1, 'Desert.jpg'),
+(13, '2015-03-26 09:12:09', 'Billabong xxl ', ' The Big Wave Awards Wipeout, Surfline Overall Performance and Women''s Best Performance nominees are in and they are nothing short of legendary.', 3, 1, 'Koala.jpg'),
+(14, '2015-03-26 09:14:18', 'Nuevo articulo333', '', 1, 1, 'Koala.jpg'),
+(17, '2015-03-26 09:32:27', 'sssseeeerrr', ' ', 3, 1, 'Koala.jpg'),
+(18, '2015-03-26 09:33:41', 'Java mola mucho', '', 3, 1, 'Koala.jpg'),
+(19, '2015-03-26 09:38:32', 'Recien creado', '', 1, 1, 'Koala.jpg'),
+(20, '2015-04-30 09:22:29', 'New faro de san temlmo', '		body		  						  	', 1, 1, 'Lighthouse.jpg'),
+(21, '2015-04-30 09:22:42', 'New faro de san temlmo', '		body		  						  	', 1, 1, 'Lighthouse.jpg'),
+(22, '2015-04-30 09:23:05', 'New faro de san temlmo', '		body		  						  	', 1, 1, 'Koala.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `titulo` varchar(250) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL DEFAULT 'Sin titulo',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `fecha`, `titulo`) VALUES
+(1, '2015-05-04 14:45:09', 'PolÃ­tica'),
+(2, '2015-05-04 14:45:09', 'Sucesos'),
+(3, '2015-05-04 14:45:39', 'Deportes'),
+(5, '2015-05-04 14:49:26', 'NuevaCategoria21');
 
 -- --------------------------------------------------------
 
@@ -86,6 +111,7 @@ INSERT INTO `usuario` (`id`, `email`, `password`, `nombre`, `rol`) VALUES
 -- Filtros para la tabla `articulo`
 --
 ALTER TABLE `articulo`
+  ADD CONSTRAINT `articulo_necesita_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `articulo_necesita_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
