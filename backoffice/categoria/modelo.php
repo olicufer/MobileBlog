ef3693a2ea18f01533690cb872464a50938caceb
ivaalfo <alfonso.aparicio.ivan@gmail.com>
2015-05-04 16:38:56 +0200

    Modificación ficheros /backoffice/categoria

--- backoffice/categoria/modelo.php
+++ backoffice/categoria/modelo.php
@@ -1,109 +1,87 @@
 <?php
+	
+	//require_once '../../core/Database.php';
 
-     error_reporting(E_ALL ^ E_DEPRECATED);
+    //error_reporting(E_ALL ^ E_DEPRECATED);
 
-	//detalle:    SELECT id,fecha, titulo FROM articulo where id=1
+	//detalle:    SELECT id, fecha, titulo FROM articulo where id=1
 	//eliminar:   DELETE FROM `articulo` WHERE `id`=1
 	//insertar:   INSERT INTO `articulo`( `titulo`) VALUES ("SSSSSSSS")
 	//modificar:  UPDATE `articulo` SET `titulo`=["el que quiera"] WHERE id=1
 
-	function insertarCategoria($categoria){
-		$cn = mysql_connect('localhost', 'root', 'root');
-		mysql_select_db('tienda', $cn);
-		
-		$sql = 'INSERT INTO `categorias`( `titulo`) VALUES ("'.$categoria.'")';
-		//echo ("sql " . $sql);
-		if ( !mysql_query( $sql, $cn) )
-		{
-			echo ("fallo insert " . $sql);
-		}
-		
-		mysql_close();
+	function insertarCategoria($titulo){
+		$db = new Database();
+		$db->connect();
+		$db->insert(
+				'categoria', 
+				array(
+						'titulo'=>$titulo 
+					)
+				);
+		$db->disconnect();
 	}
 
+	
 	/**
-	 * Eliminar un articulo por su identificador
-	 * @param $id identifacor del articulo
+	 * Eliminar un categoria por su identificador
+	 * @param $id identifacor del categoria
 	 * @return boolean si elimina true, en caso contrario false
 	 */
 	function eliminarCategoria( $id ){
-		$resul = false;
-		
-		$cn = mysql_connect('localhost', 'root', 'root');
-		mysql_select_db('tienda', $cn);
-		
-		$resul = mysql_query('DELETE FROM categorias WHERE id='.$id , $cn);
-		
-		mysql_close();
-		
-		return $resul;		
+		$db = new Database();
+		$db->connect();
+		$db->delete('categoria', 'id='.$id );
+		$db->disconnect();	
 	}
 
 
 	/**
-	 * Obtiene el detalle de un articulo por su identificador
-	 * @param $id identificador del articulo
-	 * @return articulo si existe, en caso contraio null
+	 * Obtiene el detalle de un categoria por su identificador
+	 * @param $id identificador del categoria
+	 * @return categoria si existe, en caso contraio null
 	 */
-	function detalleCategoria( $id )
-	{
-		$cn = mysql_connect('localhost', 'root', 'root');
-		mysql_select_db('tienda', $cn);
-	
-		$resultado = mysql_query('SELECT id , titulo FROM categorias where id='.$id , $cn);
-		
-		$articulos = array();
-	
-		while($articulo = mysql_fetch_assoc($resultado))
-		{
-			$articulos[] = $articulo;
-		}
-	
-		mysql_close();
+	function detalleCategoria( $id ){
+		$db = new Database();
+		$db->connect();
+		$db->select( 'categoria','*','', 'id='.$id);
+		$res = $db->getResult();
+		$db->disconnect();		
 	
 		//comrpobar longitud antes de retornar
-		if ( sizeof($articulos) > 0 ){
-			return $articulos[0];
+		if ( sizeof($res) > 0 ){
+			return $res[0];
 		}else {
 			return null;	
 		}	
 	}
 	
-	function getCategorias()
-	{
-		$cn = mysql_connect('localhost', 'root', 'root');
-		mysql_select_db('tienda', $cn);
 	
-		$resultado = mysql_query('SELECT id,nombre FROM categorias', $cn);
-		$articulos = array();
-		 
-		while($articulo = mysql_fetch_assoc($resultado))
-		{
-			$articulos[] = $articulo;
+	function getCategorias( $limit=10, $id_usuario=null ){
+		
+		$sql = "SELECT a.id, fecha, titulo FROM `categoria` as a";
+		
+		$db = new Database();
+		$db->connect();
+		
+		$where = '';
+		if( isset($id_usuario)){
+			$where = ' and id_usuario='.$id_usuario;
 		}
-		 
-		mysql_close();
-	
-		return $articulos;
+		
+		$db->sql( $sql.$where );
+		
+		$res = $db->getResult();
+		$db->disconnect();
+		return $res;
 	}
 
 	
-	
-	function modificarCategoria( $id, $nom )
-	{
-		$cn = mysql_connect('localhost', 'root', 'root');
-		mysql_select_db('tienda', $cn);
-		
-		       
-		$sql = 'UPDATE `categorias` SET `titulo`="'.$nom.'" WHERE `id`='.$id;
-		
-		if ( !mysql_query( $sql, $cn) )
-		{
-			echo ("fallo update " . $sql);
-		}
-		
-		mysql_close();
-		
+	function modificarCategoria( $id, $tit ){
+		$db = new Database();
+		$db->connect();
+		$db->update('categoria', array('titulo'=>$tit), 'id='.$id);		
+		$db->disconnect();
 	}
 
+	
 ?>
