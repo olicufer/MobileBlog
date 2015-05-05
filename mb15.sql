6c8fa4bd69a13b7cba9dd689dc6d577668d5e9f8
ivaalfo <alfonso.aparicio.ivan@gmail.com>
2015-05-05 15:46:53 +0200

    Actualizacion de la BD conforme a las categorias.

--- mb15.sql
+++ mb15.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-05-2015 a las 17:10:21
+-- Tiempo de generación: 05-05-2015 a las 15:45:04
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -44,14 +44,14 @@ CREATE TABLE IF NOT EXISTS `articulo` (
 --
 
 INSERT INTO `articulo` (`id`, `fecha`, `titulo`, `contenido`, `id_usuario`, `id_categoria`, `foto`) VALUES
-(8, '2015-03-16 16:15:35', 'Nuevo articulo sobre pec$$$$$$', '				  						  		Los PEZKEÃ‘IÃ‘IES (con nombre cientÃ­fico Pisces) son animales vertebrados acuÃ¡ticos, generalmente ectotÃ©rmicos,(regulan su temperatura a partir del medio ambiente) la mayorÃ­a de ellos recubiertos por escamas, y dotados de aletas, que permiten su movimiento continuo en los medios acuÃ¡ticos, y branquias, con las que captan el oxÃ­geno disuelto en el agua. Pisces es una superclase siendo un grupo parafilÃ©tico.        				  					  	', 1, 1, 'Jellyfish.jpg'),
-(9, '2015-03-16 16:17:15', 'PHP un lenguaje anarkiko', ' lengauje anarko  ', 3, 1, 'Desert.jpg'),
-(13, '2015-03-26 09:12:09', 'Billabong xxl ', ' The Big Wave Awards Wipeout, Surfline Overall Performance and Women''s Best Performance nominees are in and they are nothing short of legendary.', 3, 1, 'Koala.jpg'),
+(8, '2015-03-16 16:15:35', 'Nuevo articulo sobre pec$$$$$$', '				  						  						  		Los PEZKEÃ‘IÃ‘IES (con nombre cientÃ­fico Pisces) son animales vertebrados acuÃ¡ticos, generalmente ectotÃ©rmicos,(regulan su temperatura a partir del medio ambiente) la mayorÃ­a de ellos recubiertos por escamas, y dotados de aletas, que permiten su movimiento continuo en los medios acuÃ¡ticos, y branquias, con las que captan el oxÃ­geno disuelto en el agua. Pisces es una superclase siendo un grupo parafilÃ©tico.        				  					  					  	', 3, 1, 'Jellyfish.jpg'),
+(9, '2015-03-16 16:17:15', 'PHP un lenguaje anarkiko', ' lengauje anarko  ', 3, 2, 'Desert.jpg'),
+(13, '2015-03-26 09:12:09', 'Billabong xxl ', '				  		 The Big Wave Awards Wipeout, Surfline Overall Performance and Women''s Best Performance nominees are in and they are nothing short of legendary.				  	', 3, 3, 'Koala.jpg'),
 (14, '2015-03-26 09:14:18', 'Nuevo articulo333', '', 1, 1, 'Koala.jpg'),
 (17, '2015-03-26 09:32:27', 'sssseeeerrr', ' ', 3, 1, 'Koala.jpg'),
-(18, '2015-03-26 09:33:41', 'Java mola mucho', '', 3, 1, 'Koala.jpg'),
+(18, '2015-03-26 09:33:41', 'Java mola mucho', '				  						  	', 3, 5, 'Koala.jpg'),
 (19, '2015-03-26 09:38:32', 'Recien creado', '', 1, 1, 'Koala.jpg'),
-(20, '2015-04-30 09:22:29', 'New faro de san temlmo', '		body		  						  	', 1, 1, 'Lighthouse.jpg'),
+(20, '2015-04-30 09:22:29', 'New faro de san temlmo', '				  				body		  						  					  	', 2, 2, 'Lighthouse.jpg'),
 (21, '2015-04-30 09:22:42', 'New faro de san temlmo', '		body		  						  	', 1, 1, 'Lighthouse.jpg'),
 (22, '2015-04-30 09:23:05', 'New faro de san temlmo', '		body		  						  	', 1, 1, 'Koala.jpg');
 
@@ -65,6 +65,7 @@ CREATE TABLE IF NOT EXISTS `categoria` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `titulo` varchar(250) CHARACTER SET utf8 COLLATE utf8_spanish2_ci NOT NULL DEFAULT 'Sin titulo',
+  `articulos` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
@@ -72,11 +73,11 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 -- Volcado de datos para la tabla `categoria`
 --
 
-INSERT INTO `categoria` (`id`, `fecha`, `titulo`) VALUES
-(1, '2015-05-04 14:45:09', 'PolÃ­tica'),
-(2, '2015-05-04 14:45:09', 'Sucesos'),
-(3, '2015-05-04 14:45:39', 'Deportes'),
-(5, '2015-05-04 14:49:26', 'NuevaCategoria21');
+INSERT INTO `categoria` (`id`, `fecha`, `titulo`, `articulos`) VALUES
+(1, '2015-05-04 14:45:09', 'PolÃ­tica', 0),
+(2, '2015-05-04 14:45:09', 'Sucesos', 0),
+(3, '2015-05-04 14:45:39', 'Deportes', 0),
+(5, '2015-05-04 14:49:26', 'NuevaCategoria21', 0);
 
 -- --------------------------------------------------------
 
