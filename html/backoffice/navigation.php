6a2aa24a67eed6a74a568c14f32fb5b829b4a389
olicufer <olicufer@gmail.com>
2015-05-12 18:57:48 +0200

    v1.0.3: solucionada la posición del dropdown menu de user profile y logout en versión móvil.

--- html/backoffice/navigation.php
+++ html/backoffice/navigation.php
@@ -33,7 +33,7 @@
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
+                    <ul class="dropdown-menu dropdown-userfix">
                         <li>
                         	<a href="userprofile.php"><i class="fa fa-user fa-fw"></i><?php echo $perfil['nombre']?></a>
                         </li>                        
