cc2c73abc2ef2e749fd27fe7b8bb35b036a30def
iklanbide <iklanbide@HZ321638.H015110.NET>
2015-05-13 17:11:36 +0200

    Merge masterorigin

--- html/backoffice/userprofile.php
+++ html/backoffice/userprofile.php
@@ -6,7 +6,6 @@
 <div id="page-wrapper">
     <div class="row">
         <div class="col-lg-12">
-
             <h1 class="page-header">Datos Usuario - <?php echo $perfil['nombre'];?></h1>
         </div>
         <!-- /.col-lg-12 -->
@@ -48,70 +47,35 @@
         </tr>
     </table><!-- /.row -->    
 
-           <h1 class="page-header">Datos Usuario - <?php echo $perfil['nombre'];?></h1>
-         </div>
-        <!-- /.col-lg-12 -->
+    <h1 class="page-header">Datos Usuario - <?php echo $perfil['nombre'];?></h1>
+    
+    <div class="row">
+
+        <div class="form-group">
+              <label id="nombre">Nombre: </label> <?php echo $perfil['nombre'];?>
         </div>
-        <!-- /.row -->
-        <div class="row">
-				  
-				  <div class="form-group">
-				  	<label id="nombre">Nombre: </label> <?php echo $perfil['nombre'];?>
-				  </div>
-				  
-				  <div class="form-group">	
-				    <label id="email">Email: </label> <?php echo $perfil['email'];?>
-				  </div>				  	
-				  	
-				  <div class="form-group">	
-				  	<label id="password">Password: </label> <?php echo $perfil['password'];?>
-				  </div>
-				  
-				  <div class="form-group">	
-				  	<label id="rol">Rol: </label> <?php if($perfil['rol']==0){
-				  															echo "Administrador";
-				  															}else{
-				  															echo "Usuario";
-				  	}?>
-				  </div>
-				  			  
-				  
-		</div><!-- /.row -->    
+
+        <div class="form-group">	
+          <label id="email">Email: </label> <?php echo $perfil['email'];?>
+        </div>				  	
+
+        <div class="form-group">	
+              <label id="password">Password: </label> <?php echo $perfil['password'];?>
+        </div>
+
+        <div class="form-group">	
+            <label id="rol">Rol: </label>
+            <?php
+            if($perfil['rol']==0) {
+                echo "Administrador";
+            }
+            else{
+                echo "Usuario";
+            }
+            ?>
+        </div>	  
+    </div><!-- /.row -->
 
 </div><!-- <div id="page-wrapper"> -->
 
-<?php require '../../html/backoffice/footer.php'; ?>	
-
-           <h1 class="page-header">Datos Usuario - <?php echo $perfil['nombre'];?></h1>
-         </div>
-        <!-- /.col-lg-12 -->
-        </div>
-        <!-- /.row -->
-        <div class="row">
-				  
-				  <div class="form-group">
-				  	<label id="nombre">Nombre: </label> <?php echo $perfil['nombre'];?>
-				  </div>
-				  
-				  <div class="form-group">	
-				    <label id="email">Email: </label> <?php echo $perfil['email'];?>
-				  </div>				  	
-				  	
-				  <div class="form-group">	
-				  	<label id="password">Password: </label> <?php echo $perfil['password'];?>
-				  </div>
-				  
-				  <div class="form-group">	
-				  	<label id="rol">Rol: </label> <?php if($perfil['rol']==0){
-				  															echo "Administrador";
-				  															}else{
-				  															echo "Usuario";
-				  	}?>
-				  </div>
-				  			  
-				  
-		</div><!-- /.row -->    
-</div><!-- <div id="page-wrapper"> -->
-
-<?php require '../../html/backoffice/footer.php'; ?>	
-
+<?php require '../../html/backoffice/footer.php'; ?>
