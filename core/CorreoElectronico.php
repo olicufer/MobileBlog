ee36575a1e7ba38ec9e47c1e4a506647ad247f4e
gaizki <gaizkamontero@hotmail.com>
2015-05-14 16:31:15 +0200

    Nuevos métodos que son los que permiten el envio de emails a todos los usuarios cuando es creado un articulo nuevo.

--- core/CorreoElectronico.php
+++ core/CorreoElectronico.php
@@ -1,11 +1,13 @@
 <?php
-//incluimos la librería
+// incluimos la librería
 require_once ('config.php');
-require_once ('../librerias/PHPMailer/PHPMailerAutoload.php');
+require_once (CORREO_ELECTRONICO_LIBRERIA);
+require_once (MODELO_USER);
+
 
 class CorreoElectronico {
 	
-	//Atributos privados
+	// Atributos privados
 	private $phpMailer = null;
 	private $port = 465;
 	private $host = "smtp.gmail.com";
@@ -13,15 +15,17 @@ class CorreoElectronico {
 	private $mail_pass = '2015mb15';
 	private $appName = 'MobileBlog App';
 	private $type_smtp = 'ssl';
+	private $destinatarios = null;
+	private $nombre = "";
 	
-	//Constructor
-	function CorreoElectronico($debug=false){
+	// Constructor
+	function CorreoElectronico($debug = false) {
 		// crear un objeto de la libreria PHPMailer
 		$this->phpMailer = new PHPMailer ();
 		// indico a la clase que use SMTP
 		$this->phpMailer->IsSMTP ();
 		// permite modo debug para ver mensajes de las cosas que van ocurriendo
-		if($debug){
+		if ($debug) {
 			$this->phpMailer->SMTPDebug = 2;
 		}
 		// Debo de hacer autenticación SMTP
@@ -42,44 +46,69 @@ class CorreoElectronico {
 		// indicar remitente
 		$this->phpMailer->AddReplyTo ( $this->mail_user, $this->appName );
 	}
-		
-	//Metodos
-	function mandarMail($address, $nombre, $subject="MobileBlog") {
+	
+	// Metodos
+	function mandarMail($address, $nombre, $subject = "MobileBlog") {
 		// Asunto
 		$this->phpMailer->Subject = $subject;
+		
 		// Cuerpo del mensaje
-		// $this->phpMailer->MsgHTML("Hola que tal, esto es el cuerpo del mensaje!");
-		$plantilla = file_get_contents ( "../plantillas/nuevo-usuario.php" );
+		$plantilla = file_get_contents ( PLANTILLA_USER );
 		$contenido = str_replace ( "{usuario}", $nombre, $plantilla );
-		$url = WEBROOT."core/validarControlador.php?usr=".$nombre;
+		$url = WEBROOT . "core/validarControlador.php?usr=" . $nombre;
 		$contenido = str_replace ( "{url}", $url, $contenido );
 		$this->phpMailer->MsgHTML ( $contenido );
-		// $this->phpMailer->MsgHTML(file_get_contents('../plantillas/nuevo-usuario.html'));
+		
 		// indico destinatario
-		// $address = "gaizkamontero20@gmail.com";
 		$this->phpMailer->AddAddress ( $address, $nombre );
+		
 		if (! $this->phpMailer->Send ()) {
-			echo "Error al enviar: " . $mail->ErrorInfo;
+			echo "Error al enviar: " . $this->phpMailer->ErrorInfo;
+		} else {
+			echo "Mensaje enviado!";
+		}
+	}
+	function notificarArticulo($subject = "MobileBlog") {
+		// Asunto
+		$this->phpMailer->Subject = $subject;
+		
+		// Cuerpo del mensaje
+		$plantilla = file_get_contents ( PLANTILLA_ARTICULO );
+		$url = WEBROOT;
+		$contenido = str_replace ( "{url}", $url, $plantilla );
+		$this->phpMailer->MsgHTML ( $contenido );
+		$destinatarios = getUsuariosMail ();
+		
+		foreach ( $destinatarios as $usuario ) {
+			$nombre = $usuario ['nombre'];
+			$address = $usuario ['email'];
+			
+			// indico destinatario
+			$this->phpMailer->AddAddress ( $address, $nombre );
+		}
+		
+		if (! $this->phpMailer->Send ()) {
+			echo "Error al enviar: " . $this->phpMailer->ErrorInfo;
 		} else {
 			echo "Mensaje enviado!";
 		}
 	}
 	
-	function enviar(){
-		return $this->phpMailer->send();
+	// Getters y setters
+	function setAsunto($asunto) {
+		$this->phpMailer->Subject = $asunto;
 	}
-	
-	//Getters y setters
-	function setAsunto($asunto){
-		$this->phpMailer->Subject=$asunto;
+	function setNombre($nombre) {
+		$this->nombre = $nombre;
 	}
-	
-	function setDestinatario($destinatario){
-		$this->phpMailer->addAddress($destinatario);
+	function getNombre() {
+		return $this->destinatarios;
 	}
-	
-	function setCuerpo($cuerpo){
-		$this->phpMailer->msgHTML($cuerpo);
+	function getDestinatario() {
+		return $this->destinatarios;
+	}
+	function setCuerpo($cuerpo) {
+		$this->phpMailer->msgHTML ( $cuerpo );
 	}
 	
 }
